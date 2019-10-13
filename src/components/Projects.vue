6fe52d014dab305c67588b2c7b7f1d8498932142
Matt Gorzka <mgorzk@gmail.com>
2019-10-12 18:55:03 -0700

    projects edits

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,14 +1,27 @@
 <template>
-  <div>
-    <div>
-      <div class="l">
-        <a href="#">Intuit</a>
+  <div id="app">
+    <div class="box">
+      <div class="column">
+        <div class="l">
+          <a href="#">Intuit</a>
+        </div>
+        <div class="l">
+          <a href="#">Goodwill</a>
+        </div>
+        <div class="l">
+          <a href="#">SCF</a>
+        </div>
       </div>
-      <div class="l">
-        <a href="#">Goodwill</a>
-      </div>
-      <div class="l">  
-        <a href="#">SCF</a>
+      <div class="column">
+        <div class="l">
+          <a href="#">Spu</a>
+        </div>
+        <div class="l">
+          <a href="#">Ornet</a>
+        </div>
+        <div class="l">
+          <a href="#">Malgo</a>
+        </div>
       </div>
     </div>
   </div>
@@ -25,6 +38,11 @@ a {
   font-size: 48px;
 }
 
+#app {
+  width:1000px;
+  margin:100px auto;
+}
+
 body {
   display: inline-block;
   margin: 0 auto;
@@ -33,12 +51,21 @@ body {
   padding: 10px;
 }
 
-.l {
-  margin: 50px;
+.box {
+  margin:auto;
+  text-align: center;
+  width: 80%;
 }
 
-#pjs {
-  align-content: center;
+.column {
+  display: inline-block;
+  margin: 0;
+  width: 25%;
+}
+
+.l {
+  margin: 50px;
+  text-align: left;
 }
 
 </style>
