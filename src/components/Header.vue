3c2a8d28a6957ed56ef48b73b26fc25fca503f5b
Matthew Gorzka <matthew_gorzka@intuit.com>
2019-10-25 15:31:02 -0700

    css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1 id="name"><span id="firstname">matt</span><span id="lastname">gorzka</span></h1>
+  <div class="wrapper">
+    <h1 id="name slide"><span id="firstname">matt</span><span id="lastname slide">gorzka</span></h1>
   </div>
 </template>
 
@@ -24,4 +24,33 @@ export default {
 #lastname {
   color:dodgerblue
 }
+
+.wrapper {
+    position: relative;
+    overflow: hidden;
+    width: 100px;
+    height: 100px; 
+    border: 1px solid black;
+}
+
+#slide {
+    position: absolute;
+    left: -100px;
+    width: 100px;
+    height: 100px;
+    background: blue;
+    -webkit-animation: slide 0.5s forwards;
+    -webkit-animation-delay: 2s;
+    animation: slide 0.5s forwards;
+    animation-delay: 2s;
+}
+
+@-webkit-keyframes slide {
+    100% { left: 0; }
+}
+
+@keyframes slide {
+    100% { left: 0; }
+}
+
 </style>
