cdcdb9940fca8f4a14f05076479035afd433354e
Matthew Gorzka <matthew_gorzka@intuit.com>
2019-10-25 14:56:22 -0700

    css && gii

--- src/App.vue
+++ src/App.vue
@@ -58,11 +58,13 @@ export default {
 .slide-fade-enter-active {
   transition: all 1.1s ease;
 }
+
 .slide-fade-leave-active {
   transition: all 1.1s cubic-bezier(1.0, 0.5, 0.8, 1.0);
 }
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active below version 2.1.8 */ {
+
+.slide-fade-enter, 
+.slide-fade-leave-to {
   transform: translateX(10px);
   opacity: 0;
 }
