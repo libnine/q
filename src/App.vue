695248ab2307f11c88675416bcd9851d713b99fa
Matthew Gorzka <matthew_gorzka@intuit.com>
2019-10-25 15:12:48 -0700

    css && gii

--- src/App.vue
+++ src/App.vue
@@ -35,10 +35,17 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  font-family: 'Montserrat', sans-serif;
   text-align: center;
+}
+
+html {
+  margin-left: calc(100vw - 100%); 
 }
 
 .fade-enter-active,
